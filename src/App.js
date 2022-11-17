552591f189992a2dfb3b157416a3992878fbc35f
SumanMondal1 <sumanbapan2001@gmail.com>
2022-11-17 15:47:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
+const operations = ["/", "+", "-", "*"];
 function App() {
+  const [val, setVal] = useState("");
+
+  const updateVal = (x) => {
+    let lastEl = val[val.length - 1];
+    console.log(lastEl);
+    if (operations.includes(lastEl) && operations.includes(x)) {
+      console.log("true");
+    } else {
+      setVal((prevVal) => prevVal.concat(x));
+    }
+  };
+
+  const findVal = () => {
+    setVal(String(eval(val)));
+  };
+
+  const clearVal = () => {
+    setVal("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input readOnly value={val} className="textfield" />
+      <div className="boxes">
+        <button onClick={() => updateVal("7")}>7</button>
+        <button onClick={() => updateVal("8")}>8</button>
+        <button onClick={() => updateVal("9")}>9</button>
+        <button onClick={() => updateVal("/")}>/</button>
+      </div>
+      <div className="boxes">
+        <button onClick={() => updateVal("4")}>4</button>
+        <button onClick={() => updateVal("5")}>5</button>
+        <button onClick={() => updateVal("6")}>6</button>
+        <button onClick={() => updateVal("*")}>x</button>
+      </div>
+      <div className="boxes">
+        <button onClick={() => updateVal("1")}>1</button>
+        <button onClick={() => updateVal("2")}>2</button>
+        <button onClick={() => updateVal("3")}>3</button>
+        <button onClick={() => updateVal("-")}>-</button>
+      </div>
+      <div className="boxes">
+        <button onClick={() => updateVal("0")}>0</button>
+        <button onClick={() => updateVal(".")}>.</button>
+        <button onClick={findVal}>=</button>
+        <button onClick={() => updateVal("+")}>+</button>
+      </div>
+      <div className="boxes">
+        <button onClick={clearVal}>C</button>
+      </div>
     </div>
   );
 }
